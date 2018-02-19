0869186684db1192bd9ecf86d27cf5962511e72c
Kevin F <kwf5086@rit.edu>
2018-02-19 12:51:41 -0500

    Updated facade with working but maybe in the wrong format functions.

--- facade.js
+++ facade.js
@@ -1,65 +1,60 @@
-var baseUrl = 'https://cal.se.rit.edu/api/dev/';
+import $ from 'jquery'; 
+import Cookies from "js-cookie";
 
-function getInstructors(callback){
-  return $.ajax({
-    url: baseUrl + 'instructors/',
-    type: 'GET',
-    dataType: 'json',
-  })
-  .done(callback);
-}
+var baseUrl = 'http://cal.se.rit.edu/api/dev/';
 
-function createInstructor(data, callback){
-  var method = 'POST';
-  var csrftoken = Cookies.get('csrftoken');
-
-  return $.ajax({
-    url: baseUrl + 'instructors/',
-    type: method,
-    dataType: 'json',
-    data: data,
-    beforeSend: function(xhr){
-      if (!this.crossDomain){
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-    },
-  })
-
-  .done(callback);
-}
-
-function getCourses(callback){
-return $.ajax({
-    url: baseUrl + 'courses/',
-    type: 'GET',
-    dataType: 'json',
-  })
-  .done(callback);
-}
-
-function createCourse(data, callback){
-  var method = 'POST';
-  var csrftoken = Cookies.get('csrftoken');
-
-  return $.ajax({
-    url: baseUrl + 'courses/',
-    type: method,
-    dataType: 'json',
-    data: data,
-    beforeSend: function(xhr){
-      if (!this.crossDomain){
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-    },
-  })
-  .done(callback);
-}
-
-function scheduleCourse(data, callback){
-  var method = 'POST';
-  var csrftoken = Cookies.get('csrftoken');
-
+export const getInstructors = () =>{
+  var result = [];
   $.ajax({
+    url: baseUrl + 'instructors/',
+    type: 'GET',
+    async: false,
+    dataType: 'json',
+    success:function(data){
+      result = data;
+    }
+  })
+  return result;
+}
+
+export const getCourses = () =>{
+  var result = [];
+  $.ajax({
+    url: baseUrl + 'courses/',
+    type: 'GET',
+    async: false,
+    dataType: 'json',
+    success:function(data){
+      result = data;
+    }
+  })
+  return result;
+}
+
+export const createCourse = (data, callback) =>{
+  var method = 'POST';
+  var csrftoken = Cookies.get('csrftoken');
+
+  return $.ajax({
+    url: baseUrl + 'courses/',
+    type: method,
+    dataType: 'json',
+    data: data,
+    beforeSend: function(xhr){
+      if (!this.crossDomain){
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    },
+    error: basicErrorFunction,
+    success: callback,
+  })
+}
+
+export const scheduleCourse = (data)=>{
+  var method = 'POST';
+  var csrftoken = Cookies.get('csrftoken');
+
+  return $.ajax({
     url: baseUrl + 'schedulecourse/',
     type: method,
     dataType: 'json',
@@ -69,11 +64,21 @@ function scheduleCourse(data, callback){
         xhr.setRequestHeader("X-CSRFToken", csrftoken);
       }
     },
+    error: basicErrorFunction,
+    success: scheduleSuccess
   })
-  .done(callback);
 }
 
-function checkAvailability(data, callback){
+function scheduleSuccess(data){
+  window.location.replace("/Confirmation");
+}
+
+
+function basicErrorFunction(xhr, status, error){
+  alert("An arror occured: " + xhr.responseText);
+}
+
+export const checkAvailability = (data, callback)=>{
   var method = 'POST';
   var csrftoken = Cookies.get('csrftoken');
 
@@ -82,6 +87,7 @@ function checkAvailability(data, callback){
     type: method,
     dataType: 'json',
     data: data,
+    success: colorRooms,
     beforeSend: function(xhr){
       if (!this.crossDomain){
         xhr.setRequestHeader("X-CSRFToken", csrftoken);
@@ -91,6 +97,20 @@ function checkAvailability(data, callback){
   .done(callback);
 }
 
-function print(data){
-  console.log(data);
+function colorRooms (data){
+  var temp;
+  var roomsToCheck = ["A020", "A060", "A068", "A070", "A076", "A077", "A078", "A079", "A081", "A083", "A085",
+                      "A087", "A090", "A093", "A094", "A095", "A096", "A097", "A098", "A099", "A100", "A110",
+                      "A120", "A140", "A141", "A145", "A155", "A171", "A1118"];
+  var currCheck;
+  var dataArray = data.data;
+  for(var i=0; i<dataArray.length; i++){
+    temp = dataArray[i];
+    var assocBool = temp[roomsToCheck[i]];
+    if(assocBool){
+      $("#"+roomsToCheck[i]).attr("fill", "green");
+    }else{
+      $("#"+roomsToCheck[i]).attr("fill", "red");
+    }
+  }
 }
